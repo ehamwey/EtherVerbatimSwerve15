44fa476015ee9926769bfd3a34fff0481e852811
EmileHamwey <ehamwey@team3467.org>
2015-04-29 18:11:40 -0400

    removed junk line of code

--- Swerve4-27/src/org/usfirst/frc/team3467/robot/Robot.java
+++ Swerve4-27/src/org/usfirst/frc/team3467/robot/Robot.java
@@ -93,7 +93,6 @@ public class Robot extends SampleRobot {
     	fls = Math.sqrt(b*b + d*d);
     	rls = Math.sqrt(a*a + d*d);
     	rrs = Math.sqrt(a*a + c*c);
-    	SmartDashboard.get
     	fra = Math.atan2(b,c) * 180/Math.PI;
     	fla = Math.atan2(b,d) * 180/Math.PI;
     	rra = Math.atan2(a,d) * 180/Math.PI;
